47237385f302509c7b3dc9cafceac23c6c901bed
김경석 <kevin_kim@kevin.local>
2022-10-11 21:41:06 +0900

    상품검색내 옵션검색 추가

--- get_data/godo_product.js
+++ get_data/godo_product.js
@@ -1,21 +1,32 @@
 'use strict'
 
+const schedule = require('node-schedule');
+const { DateTime } = require("luxon");
+
 const util = require("../public/commonUtil.js");
-const companyNm = '㈜원앤드원쇼핑'; //no 635
-const scmNo = 635
-const startDate = '2022-09-22';
-const endDate = '2022-09-22';
-const goodsNo = 1000008154;
+
+const startDate = DateTime.now().minus({days: 1}).toFormat('yyyy-LL-dd');
+const endDate = DateTime.now().toFormat('yyyy-LL-dd');
+const pageNo = 1;
 
 getProduct();
 
 async function getProduct() {
     const options = { method: 'POST',
-        url: `${util.param.main_url}/goods/Goods_Search.php?${util.param.main_key}&cateCd=064`
+        url: `${util.param.main_url}/goods/Goods_Search.php?${util.param.main_key}&searchDateType=regDt&startDate=${startDate}&endDate=${endDate}&page=${pageNo}`
     };
 
     const xmlRowData = await util.xmlData(options);
     const jsonData = await util.parseXml(xmlRowData);
     const goodsData = jsonData.data.return[0].goods_data;
-    console.log(goodsData);
+    for(let i = 0; i < goodsData.length; i++) {
+        const r = goodsData[i];
+        const productData = [r.goodsNo[0],r.goodsNm[0],r.scmNo[0],r.scmCommissionGrade[0],r.goodsCd[0],r.brandCd[0],
+            r.fixedPrice[0],r.goodsPrice[0],r.deliverySno[0],r.allCateCd[0]];
+        for(let j = 0; j < r.optionData[0].length; j++) {
+            const s = r.optionData[0][j];
+            const optionData = [s.sno[0],s.goodsNo[0],s.optionPrice[0],s.optionCode[0],
+                s.optionValue1[0],s.optionValue2[0],s.optionValue3[0],s.optionValue4[0]];
+        }
+    }
 }
